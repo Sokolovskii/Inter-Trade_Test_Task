66e6630204e0e14ebc6b1428d56c7cc7d5852548
Alexander Sokolovskii <sokolovskii.alex2@gmail.com>
2026-04-28 03:28:04 +1000

    Предварительно отрефакторил репозиторий

---
+++ Inter-Trade_Test_Task.DAL/DBL/SQLiteConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SQLite;
+
+namespace Inter_Trade_Test_Task.DAL.DBL
+{
+    public class SQLiteConnectionFactory : IDbConnnectionFactory
+    {
+        private readonly string _connectionString;
+        public SQLiteConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public async Task<SQLiteConnection> GetConnection()
+        {
+            var connection = new SQLiteConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
